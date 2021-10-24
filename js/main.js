e432edd8879f7fa00fdcc36b1b62f692d550b8c0
flashback2k14 <yeahdev@gmail.com>
2021-10-24 21:50:22 +0200

    implement

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 (() => {
-  console.log('Ready to rock!');
+  const UI = {
+    cardItems: [...document.querySelectorAll('.card-item')],
+  };
+
+  const _handleCardContentVisibility = (ev) => {
+    const title = ev.currentTarget.children[0];
+    title.classList.toggle('active');
+    const content = ev.currentTarget.children[1];
+    if (content.classList.contains('hidden')) {
+      content.classList.remove('hidden');
+      content.classList.add('visible');
+    } else {
+      content.classList.remove('visible');
+      content.classList.add('hidden');
+    }
+  };
+
+  const _initHandler = () => {
+    UI.cardItems.forEach((cardItem) => {
+      cardItem.addEventListener('click', _handleCardContentVisibility);
+    });
+  };
+
+  const init = () => {
+    _initHandler();
+
+    window.addEventListener('beforeunload', () => {
+      UI.cardItems.forEach((cardItem) => {
+        cardItem.removeEventListener('click', _handleCardContentVisibility);
+      });
+    });
+  };
+
+  init();
 })();
